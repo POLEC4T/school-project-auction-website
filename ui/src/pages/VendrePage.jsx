b18be835d18f209b411e0e9debd88c66e067889a
adrien <adrien.ligne@etu.univ-grenoble-alpes.fr>
2023-01-12 00:00:11 +0100

    message explicite refus page vendeur

--- ui/src/pages/VendrePage.jsx
+++ ui/src/pages/VendrePage.jsx
@@ -30,7 +30,7 @@ function VendrePage() {
         return (
             <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
                 <h1 className="text-2xl font-semibold md:text-3xl">Accès refusé</h1>
-                <p className="mt-4 mb-8 dark:text-black font-semibold">{data.message}</p>
+                <p className="mt-4 mb-8 dark:text-black font-semibold">Vous devez être connecté avec un compte vendeur pour accéder à cette page</p>
                 <Link to="/" className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Back to homepage</Link>
             </div>
         );
