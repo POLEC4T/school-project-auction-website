9f659f40d7edb21296fc6ae41c24d2eb4fdf755f
adrien <adrien.ligne@etu.univ-grenoble-alpes.fr>
2023-01-13 14:32:28 +0100

    page profil

--- ui/src/pages/ProfilePage.jsx
+++ ui/src/pages/ProfilePage.jsx
@@ -27,9 +27,9 @@ function ProfilePage() {
             setIsConnected(true);
             setRole(response.data.role);
             setData(response.data.user);
-            if(response.data.user.banniere !== null) setBanniere(response.data.user.banniere)
+            setBanniere(response.data.user.banniere)
             setIsLoading(false)
-            if(response.data.user.pdp !== null) setAvatar(require(response.data.user.pdp));
+            if(response.data.user.pdp !== null) setAvatar(require(`${response.data.user.pdp}`));
  
         }).catch((error) => {
             setIsConnected(false);
@@ -47,29 +47,29 @@ function ProfilePage() {
             <>
             <NavBar/>
             {banniere === null ? (
-                <section  class="top sm:h-80 h-40 relative justify-center items-center bg-center bg-banniere bg-no-repeat bg-100 flex ">
-                    <button name="personnaliser" class="sm:text-xl text-sm text-black bg-zinc-300 hover:bg-zinc-200 absolute sm:top-5 top-2 sm:right-5 right-2 rounded-xl px-4 py-1">personnaliser</button>
-                    <h1 class="font-gowun text-6xl text-white">{data.login}</h1>
+                <section  className="top sm:h-80 h-40 relative justify-center items-center bg-center bg-banniere bg-no-repeat bg-100 flex ">
+                    <button name="personnaliser" className="sm:text-xl text-sm text-black bg-zinc-300 hover:bg-zinc-200 absolute sm:top-5 top-2 sm:right-5 right-2 rounded-xl px-4 py-1">personnaliser</button>
+                    <h1 className="font-gowun text-6xl text-white">{data.login}</h1>
                 </section>
             ) : (
-                <section style={{backgroundImage: `url(${banniere})`}}  class="top sm:h-80 h-40 relative justify-center items-center bg-center bg-no-repeat bg-100 flex ">
-                    <button name="personnaliser" class="sm:text-xl text-sm text-black bg-zinc-300 hover:bg-zinc-200 absolute sm:top-5 top-2 sm:right-5 right-2 rounded-xl px-4 py-1">personnaliser</button>
-                    <h1 class="font-gowun text-6xl text-white">{data.login}</h1>
+                <section style={{backgroundImage: `url(${banniere})`}}  className="top sm:h-80 h-40 relative justify-center items-center bg-center bg-no-repeat bg-100 flex ">
+                    <button name="personnaliser" className="sm:text-xl text-sm text-black bg-zinc-300 hover:bg-zinc-200 absolute sm:top-5 top-2 sm:right-5 right-2 rounded-xl px-4 py-1">personnaliser</button>
+                    <h1 className="font-gowun text-6xl text-white">{data.login}</h1>
                 </section>
             )}
 
             <ProfileNav role={role} onClickButton={handleClickedButton}/>
-            <section class="bottom-profil sm:px-28 px-4">
+            <section className="bottom-profil sm:px-28 px-4">
 
-            <div class="middle1 pt-10 flex flex-row justify-between">
+            <div className="middle1 pt-10 flex flex-row justify-between">
 
-                <div class="profil flex flex-row">
+                <div className="profil flex flex-row">
 
-                    <img class="sm:w-24 w-16 sm:h-24 h-16 rounded-full" src={avatar} alt="photo de profil"/>
+                    <img className="sm:w-24 w-16 sm:h-24 h-16 rounded-full" src={avatar} alt="photo de profil"/>
 
-                    <div class="nom flex flex-col justify-around ml-5">
+                    <div className="nom flex flex-col justify-around ml-5">
 
-                        <p class="sm:text-4xl text-2xl">{data.login}</p>
+                        <p className="sm:text-4xl text-2xl">{data.login}</p>
                         <p>{clickedButton}</p>
                     </div>
                 </div>
